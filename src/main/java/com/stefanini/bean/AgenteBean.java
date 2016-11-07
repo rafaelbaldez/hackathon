5fbffb70042592d289c6244ce785148860e931d2
rafaelbaldez <rafaelbaldez@gmail.com>
2016-11-07 16:37:29 -0200

    adicionando a bean agente

---
+++ src/main/java/com/stefanini/bean/AgenteBean.java
@@ -0,0 +1,45 @@
+package com.stefanini.bean;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.stefanini.model.Agente;
+import com.stefanini.service.AgenteService;
+import com.stefanini.service.DenunciaService;
+
+@Named("agenteMB")
+@SessionScoped
+public class AgenteBean implements Serializable {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+    private Agente agente;
+    
+    
+
+    public String chamar() {
+        return "/teste";
+    }
+
+
+
+	public Agente getAgente() {
+		return agente;
+	}
+
+
+
+	public void setAgente(Agente agente) {
+		this.agente = agente;
+	}
+
+	
+
+}
---
+++ src/main/java/com/stefanini/repository/AgenteRepository.java
@@ -0,0 +1,31 @@
+package com.stefanini.repository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.stefanini.model.Agente;
+import com.stefanini.model.Denuncia;
+
+public class AgenteRepository {
+	@Inject
+	private EntityManager manager;
+
+	public void incluir(Agente agente) {
+		this.manager.persist(agente);
+	}
+
+	public void altera(Agente agente) {
+		this.manager.merge(agente);
+	}
+
+	public Agente busca(Integer id) {
+		return this.manager.find(Agente.class, id);
+	}
+
+	public List<Agente> lista() {
+		return this.manager.createQuery("select c from agente c", Agente.class)
+				.getResultList();
+	}
+}
